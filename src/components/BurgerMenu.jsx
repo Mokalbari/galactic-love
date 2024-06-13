c2dc01848ae718b53765b013474eb37598043d0c
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-13 16:46:24 +0200

    assemble whole page + fix mistake in Header import

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,21 +1,20 @@
-import './Header.css';
-import 'boxicons';
-function BurgerMenu ({filter}) {
-    return (
-        <nav className="burger-menu">
-            <i class='bx bx-user' type="button" onClick={() => filter()}/>
-            <i class='bx bx-user' type="button" onClick={() => filter()}/>
-            <i class='bx bx-user' type="button" onClick={() => filter()}></i>
-            <i class='bx bx-user' type="button" onClick={() => filter()}></i>
-            <i class='bx bx-user' type="button" onClick={() => filter()}></i>
-            {/* <button type="button" onClick={() =>filter() }>age</button>
+import './Header.css'
+
+function BurgerMenu({ filter }) {
+  return (
+    <nav className="burger-menu">
+      {/* <i class="bx bx-user" type="button" onClick={() => filter()} />
+      <i class="bx bx-user" type="button" onClick={() => filter()} />
+      <i class="bx bx-user" type="button" onClick={() => filter()}></i>
+      <i class="bx bx-user" type="button" onClick={() => filter()}></i>
+      <i class="bx bx-user" type="button" onClick={() => filter()}></i> */}
+      {/* <button type="button" onClick={() =>filter() }>age</button>
             <button type="button" onClick={() =>filter() }>race</button>
             <button type="button" onClick={() =>filter() }>affiliation</button>
             <button type="button" onClick={() =>filter() }>genre</button>
             <button type="button" onClick={() =>filter() }>planete</button> */}
-
-        </nav>
-    );
+    </nav>
+  )
 }
 
-export default BurgerMenu;
+export default BurgerMenu
--- src/components/CardListAPI.jsx
+++ src/components/CardListAPI.jsx
@@ -1,16 +1,15 @@
-
 import { useEffect, useState } from 'react'
 import CarteCaroussel from './CarteCaroussel'
-import "../styles/CardListApi.css"
+import '../styles/CardListApi.css'
 
-function CardListAPI({character}) {
-  
-
-  return (<div className='carte-liste'>
-    {character.map(element =>
-      <CarteCaroussel key={element.id} {...element} />
-    )}
-  </div>)
-
+function CardListAPI({ character }) {
+  return (
+    <div className="carte-liste">
+      {character.map(element => (
+        <CarteCaroussel key={element.id} {...element} />
+      ))}
+    </div>
+  )
+}
 
 export default CardListAPI
