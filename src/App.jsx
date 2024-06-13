ec348c50735e39ac5e4adda5b8da9fccddedc75d
Aur3liaP <pic.aurelia@gmail.com>
2024-06-13 16:17:16 +0200

    adding CSS to Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 import CardListAPI from './components/CardListAPI'
 import Menu from './components/Menu'
 
 function App() {
+  const [character, setCharacter] = useState([])
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await fetch(
+        'https://miadil.github.io/starwars-api/api/all.json',
+      )
+      const data = await response.json()
+      console.log(data)
+      setCharacter(data)
+    }
+    fetchAPI()
+  }, [])
+
   return (
     <>
-      <Menu />
+      <Menu/>
+      <CardListAPI character={character}/>
     </>
   )
 }
