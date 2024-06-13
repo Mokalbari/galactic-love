9052c4aee63c4e77879a296fb5322b3b85478193
Aur3liaP <pic.aurelia@gmail.com>
2024-06-13 16:28:23 +0200

    conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import './App.css'
-import { useEffect, useState } from 'react'
-import CardListAPI from './components/CardListAPI'
-import Menu from './components/Menu'
+import CardListAPI from './assets/components/CardListAPI'
 
 function App() {
-  const [character, setCharacter] = useState([])
-
-  useEffect(() => {
-    const fetchAPI = async () => {
-      const response = await fetch(
-        'https://miadil.github.io/starwars-api/api/all.json',
-      )
-      const data = await response.json()
-      console.log(data)
-      setCharacter(data)
-    }
-    fetchAPI()
-  }, [])
-
   return (
     <>
-      <Menu/>
-      <CardListAPI character={character}/>
+      <CardListAPI />
     </>
   )
 }
