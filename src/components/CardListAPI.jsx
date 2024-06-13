3aa48dce43fd6c0e6fc08493eca2e8247dd7e900
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-13 16:19:16 +0200

    traces de recherches pour le responsive

--- src/components/CardListAPI.jsx
+++ src/components/CardListAPI.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react'
 import CarteCaroussel from './CarteCaroussel'
 import "../styles/CardListApi.css"
 
 function CardListAPI({character}) {
   
-
+   var lorys = ""
   return (<div className='carte-liste'>
     {character.map(element =>
       <CarteCaroussel key={element.id} {...element} />
--- src/components/CarteCaroussel.jsx
+++ src/components/CarteCaroussel.jsx
@@ -1,23 +1,23 @@
-import jabba from "../assets/Jabba-The-Hutt.png"
-import "../styles/CarteCaroussel.css"
+import jabba from '../assets/Jabba-The-Hutt.png'
+import '../styles/CarteCaroussel.css'
 
-const CarteCaroussel = ({image, name}) =>{
-    return(
-        <div className="carte-caroussel">
-            <img src={image} alt={name} />
-            <div className="wrapper">
-                <h3>{name}</h3>
-                <div id="affiliation">
-                    <p>affiliation</p>
-                    <ul>
-                        <li>les méchants</li>
-                        <li>la mafia</li>
-                        <li>la gauche communiste</li>
-                    </ul>
-                </div>
-            </div>
+const CarteCaroussel = ({ image, name }) => {
+  return (
+    <div className="carte-caroussel">
+      <img className="carte-img" src={image} alt={name} />
+      <div className="wrapper">
+        <div id="affiliation">
+          <h3>{name}</h3>
+          <p>affiliation</p>
+          <ul>
+            <li>les méchants</li>
+            <li>la mafia</li>
+            <li>la gauche communiste</li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default CarteCaroussel
